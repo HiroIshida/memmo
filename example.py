5f5193910b75f295e2125f48649cf554426cc193
HiroIshida <spitfire.docomo@gmail.com>
2022-12-22 21:51:55 +0900

    example: plot and compare each method

--- example.py
+++ example.py
@@ -1,4 +1,7 @@
 import tqdm
+from pathlib import Path
+import pickle
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 from square.optimizer import OptimizationBasedPlanner
@@ -64,25 +67,46 @@ if __name__ == "__main__":
     sdf3 = CircleObstacle(np.array([0.7, 0.4]), 0.1)
     world = SquareWorld((sdf1, sdf2, sdf3))
 
-    traj_list = generate_data(500, world)
+    cache_path = Path("/tmp/memmo_traj_list.cache")
+    if cache_path.exists():
+        with cache_path.open(mode = "rb") as f:
+            traj_list = pickle.load(f)
+    else:
+        traj_list = generate_data(1000, world)
+        with cache_path.open(mode = "wb") as f:
+            pickle.dump(traj_list, f)
+    traj_list = traj_list[:300]
     X, Y = traj_list_to_XY(traj_list)
 
-    start, goal = pick_start_goal(world)
+    #start, goal = pick_start_goal(world)
+    start = np.array([0.1, 0.1])
+    goal = np.array([0.9, 0.9])
     x_query = np.hstack([start, goal])
-    # regr = NN_Regressor.fit(X, Y)
-    # regr = Straight_Regressor.fit(X, Y)
-    regr = GPy_Regressor.fit(X, Y)
 
-    y = regr.predict(x_query)
-    y = y.reshape(-1, 2)
+    # fit all regressors
+    regressor_table = {
+            "nn": NN_Regressor.fit(X, Y),
+            "std": Straight_Regressor.fit(X, Y),
+            "gpr": GPy_Regressor.fit(X, Y),
+    }
 
-    init_traj_est = Trajectory(list(y))
+    # prediction by all regressors
+    result_table = {
+            key: regr.predict(x_query) 
+            for key, regr in regressor_table.items()}
 
-    # visualize
-    fig, ax = world.visualize()
-    init_traj_est.visualize((fig, ax), "bo-", lw=2.0, ms=2.0)
+    # visualize problem setting
+    fig, ax = world.visualize(with_contourf = False)
     for traj in traj_list:
         traj.visualize((fig, ax), "ro-", lw=0.4, ms=0.4)
     ax.plot(start[0], start[1], "ko")
     ax.plot(goal[0], goal[1], "ko")
+
+    # visualize prediction results
+    plot_styles = ["bo-", "ko-", "co-"]
+    for regr_name, plot_style in zip(result_table.keys(), plot_styles):
+        result_traj = result_table[regr_name]
+        traj = Trajectory(list(result_traj.reshape(-1, 2)))
+        traj.visualize((fig, ax), plot_style, lw=2.0, ms=2.0, label=regr_name)
+    plt.legend(loc="upper left")
     plt.show()
